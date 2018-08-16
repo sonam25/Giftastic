597a100871f12b7ef6c93bbc9362255d097be82b
Sonam Mankame <sonamrohit.ms25@gmail.com>
2018-08-16 15:16:28 -0400

    added html css and js file

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,54 +1,96 @@
   // Adding click event listen listener to all buttons
-  var animalAr = ["dog","rat","rabbit","lion","tiger","cow","snake"];
-   
-  for(var i =0; i<animalAr.length;i++){
-    $("#animals").append("<button data-animal =" + animalAr[i] +">" + animalAr[i] + "</button>");
-  // console.log(animalAr[i]);
+  var animalAr = ["Wild dog", "Wolf","Monkey", 
+  " Fox","Black Bear"," Polar Bear ","Otter","Ferret ",
+  "Snake","Girafee","Lion","Tiger",
+  "Elephant","Cheetah","Pandat","Gorilla","Camel","Deer","Meerkat" ];
+  
+    for(var i =0; i<animalAr.length;i++)
+  {
+       $("#animals").append("<button class = 'Button' data-animal =" + animalAr[i] +" >" + animalAr[i] + "</button>");
+  }
+ //add on buttonclick function
+       onbuttonclick();
 
-}
-   
-  $("button").on("click", function() {
-    // Grabbing and storing the data-animal property value from the button
-    var animal = $(this).attr('data-animal');
-      //console.log(animal);
-    // Constructing a queryURL using the animal name
-    var queryURL = "https://api.giphy.com/v1/gifs/search?api_key=NqGXiZ1JTV8o4ZAtubeHmx4GmVFpdLSh&q=" + animal+"&limit=10&offset=0&rating=G&lang=en";
+  $("#submit").on("click",function()
+  {
+      var userInput = $('#Data').val();
+      $('#animals').append("<button class = 'Button' data-animal =" + userInput + " >" + userInput + "</button>");
+      //console.log(animalAr);
+      animalAr.push(userInput);
+      
+      $('#Data').val('');
+      //add on buttonclick function
+      onbuttonclick();
+  });
+  
 
-    // Performing an AJAX request with the queryURL
-    $.ajax({
+  function onbuttonclick()
+  {
+    
+    $(".Button").on("click", function() {
+      // Grabbing and storing the data-animal property value from the button
+      var animal = $(this).attr('data-animal');
+      
+      // Constructing a queryURL using the animal name
+      var queryURL = "https://api.giphy.com/v1/gifs/search?api_key=NqGXiZ1JTV8o4ZAtubeHmx4GmVFpdLSh&q=" + animal+"&limit=10&offset=0&rating=G&lang=en";
+
+      // Performing an AJAX request with the queryURL
+      $.ajax({
       url: queryURL,
       method: "GET"
-    })
+      })
       // After data comes back from the request
-      .then(function(response) {
-        console.log(queryURL);
-
-        console.log(response);
+        .then(function(response)
+       {
+        
         // storing the data from the AJAX request in the results variable
-        var results = response.data;
+            var results = response.data;
 
-        // Looping through each result item
-        for (var i = 0; i < results.length; i++) {
+            // Looping through each result item
+            for (var i = 0; i < results.length; i++) 
+            {
+            // Creating and storing a div tag
+                var animalDiv = $("<div class = 'animalstyle'>");
 
-          // Creating and storing a div tag
-          var animalDiv = $("<div class = 'animalstyle'>");
+                // Creating a paragraph tag with the result item's rating
+                var p = $("<p>").text("Rating: " + results[i].rating);
 
-          // Creating a paragraph tag with the result item's rating
-          var p = $("<p>").text("Rating: " + results[i].rating);
+                //creat image variable to hold image data
+                var image = '<img src= " ' + results[i].images.fixed_height_still.url +
+                '" data-still=" ' + results[i].images.fixed_height_still.url +
+                ' " data-animate=" ' + results[i].images.fixed_height.url + '" data-state="still" class="movImage">';
 
-          // Creating and storing an image tag
-          var animalImage = $("<img>");
-          // Setting the src attribute of the image to a property pulled off the result item
-          animalImage.attr("src", results[i].images.fixed_height.url);
+                image = '<div class="col-md-4">' + image + "</div>";
 
-          // Appending the paragraph and image tag to the animalDiv
-          animalDiv.append(p);
-          animalDiv.append(animalImage);
+                // Appending the paragraph and image tag to the animalDiv
+                animalDiv.append(p);
+                animalDiv.append(image);
 
-          // Prependng the animalDiv to the HTML page in the "#gifs-appear-here" div
-          $("#gifs-appear-here").append(animalDiv);
-        }
-       
-      }); $("#gifs-appear-here").empty();
-  });
- 
+
+                // Prependng the animalDiv to the HTML page in the "#gifs-appear-here" div
+                $("#gifs-appear-here").append(animalDiv);
+
+            }
+            //add imageclick function
+          imageClick();
+
+        });
+
+       $("#gifs-appear-here").empty();
+
+    });
+}
+
+function imageClick(){
+  $('.movImage').on('click', function() {
+    var state = $(this).attr('data-state');
+    if (state == 'still') {
+        $(this).attr('src', $(this).attr("data-animate"));
+        $(this).attr('data-state', 'animate');
+    } else {
+        $(this).attr('src', $(this).attr("data-still"));
+        $(this).attr('data-state', 'still');
+    }
+
+}); 
+}
